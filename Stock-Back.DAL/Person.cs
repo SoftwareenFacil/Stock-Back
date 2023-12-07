e92d65b4f916f181e04a985f227c78ae2dbc1551
Nicolas Vargas <vargastapianicolas@gmail.com>
2023-12-07 19:32:47 -0300

    Necesary NuGet Packages Installed

---
+++ Stock-Back.DAL/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Back.DAL
+{
+    public class Person
+    {
+        public string HelloWorld {  get; set; }
+        public Person(string helloWorld)
+        {
+            HelloWorld = helloWorld;
+        }
+    }
+}
